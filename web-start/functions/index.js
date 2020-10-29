d67673eeec66dce78ec41796e5552cdb681dd8ca
yb-ctrlv <yb.ctrlv@gmail.com>
2020-10-29 14:09:43 +0900

    complete web codelab

--- web-start/functions/index.js
+++ web-start/functions/index.js
@@ -17,9 +17,77 @@
 // Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+//Import the Firebase SDK for Google Cloud Functions.
+const functions = require('firebase-functions');
+//Import and initialize the Firebase Admin SDK;
+const admin = require('firebase-admin');
+
+const Vision = require('@google-cloud/vision');
+const vision = new Vision();
+const spawn = require('child-process-promise').spawn;
+
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+admin.initializeApp();
+
 
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
+    console.log('A new user signed in for the first time.');
+    const fullName = user.displayName || 'Anonymous';
 
+    // Saves the new welcome messaage into the database
+    // which then displays it in the FriendlyChat clients.
+    await admin.firestore().collection('messages').add({
+        name: 'Firebase Bot',
+        profilePicUrl: '/images/firebase-logo.png',
+        text: `${fullName} signed in for the first time! Welcom!`,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    });
+    console.log('Welcome message written to database');
+});
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
+exports.blurOffensiveImages = functions.runWith({memory: '2GB'}).storage.object().onFinalize(
+    async (object) => {
+        const image = {
+            source: {imageUri: `gs:/${object.bucket}/${object.name}`},
+        };
+
+        //Check the image content using the Cloud Vision API.
+        const batchAnnotateImagesResponse = await vision.safeSearchDetection(image);
+        const safeSearchResult = batchAnnotateImagesResponse[0].safeSearchAnnotation;
+        const Likelihood = Vision.types.Likelihood;
+        if(Likelihood[safeSearchResult.adult] >= Likelihood.LIKELY ||
+            Likelihood[safeSearchResult.violence] >= Likelihood.LIKELY){
+                console.log('The image', object.name, 'has been detected as inappropriate.');
+                return blurImage(object.name);
+            }
+            console.log('The image', object.name, 'has been detected as OK.');
+    });
+
+
+async function blurImage(filePath){
+    const tempLocalFile = path.join(os.tmpdir(), path.basename(filePath));
+    const messageId = filePath.split(path.seq)[1];
+    const bucket = admin.storage().bucket();
+
+    await bucket.file(filePath).download({destination: tempLocalFile});
+    console.log('Image has been downloaded to', tempLocalFile);
+
+    await spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x24', tempLocalFile]);
+    console.log('Image has been blurred');
+  
+    await bucket.upload(tempLocalFile, {destination: filePath});
+    console.log('Blurred image has been uploaded to', filePath);
+
+    fs.unlinkSync(tempLocalFile);
+    console.log('Delete local file.');
+
+    await admin.firestore().collection('messages').doc(messageId).update({moderated:true});
+    console.log('Marked the image as moderated in the database');
+}
 
 // TODO(DEVELOPER): Write the sendNotifications Function here.
--- web-start/public/scripts/main.js
+++ web-start/public/scripts/main.js
@@ -17,59 +17,124 @@
 
 // Signs-in Friendly Chat.
 function signIn() {
-  alert('TODO: Implement Google Sign-In');
+  // alert('TODO: Implement Google Sign-In');
   // TODO 1: Sign in Firebase with credential from the Google user.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 }
 
 // Signs-out of Friendly Chat.
 function signOut() {
   // TODO 2: Sign out of Firebase.
+  firebase.auth().signOut();
 }
 
 // Initiate firebase auth.
 function initFirebaseAuth() {
   // TODO 3: Initialize Firebase.
+  firebase.auth().onAuthStateChanged(authStateObserver);
 }
 
 // Returns the signed-in user's profile Pic URL.
 function getProfilePicUrl() {
   // TODO 4: Return the user's profile pic URL.
+  return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
 }
 
 // Returns the signed-in user's display name.
 function getUserName() {
   // TODO 5: Return the user's display name.
+  return firebase.auth().currentUser.displayName;
 }
 
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
   // TODO 6: Return true if a user is signed-in.
+  return !!firebase.auth().currentUser;
 }
 
 // Saves a new message on the Firebase DB.
 function saveMessage(messageText) {
   // TODO 7: Push a new message to Firebase.
+  return firebase.firestore().collection('messages').add({
+    name: getUserName(),
+    text: messageText,
+    profilePicUrl: getProfilePicUrl(),
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  }).catch(function(error){
+    console.error('Error writing new message to database',error);
+  })
 }
 
 // Loads chat messages history and listens for upcoming ones.
 function loadMessages() {
   // TODO 8: Load and listens for new messages.
+  var query = firebase.firestore()
+                .collection('messages')
+                .orderBy('timestamp','desc')
+                .limit(12);
+  query.onSnapshot((snapshot)=>{
+    snapshot.docChanges().forEach(change => {
+      if(change.type === 'removed'){
+        deleteMessage(change.doc.id);
+      }else{
+        var message = change.doc.data();
+        displayMessage(change.doc.id, message.timestamp, message.name, message.text, message.profilePicUrl, message.imageUrl);
+      }
+    })
+  })                
 }
 
 // Saves a new message containing an image in Firebase.
 // This first saves the image in Firebase storage.
 function saveImageMessage(file) {
   // TODO 9: Posts a new image as a message.
+  firebase.firestore().collection('messages').add({
+    name: getUserName(),
+    imageUrl: LOADING_IMAGE_URL,
+    profilePicUrl: getProfilePicUrl(),
+    timestamp : firebase.firestore.FieldValue.serverTimestamp()
+  }).then(function(messageRef) {
+    var filePath = firebase.auth().currentUser.uid + '/' + messageRef.id + '/' + file.name;
+    return firebase.storage().ref(filePath).put(file).then(function(fileSnapshot){
+      return fileSnapshot.ref.getDownloadURL().then((url)=>{
+        return messageRef.update({
+          imageUrl : url,
+          storageUri : fileSnapshot.metadata.fullPath
+        });
+      });
+    });
+  }).catch(function(error) {
+    console.error('There was an error uploading a file to Cloud Storage:' + error);
+  });
 }
 
 // Saves the messaging device token to the datastore.
 function saveMessagingDeviceToken() {
   // TODO 10: Save the device token in the realtime datastore
+  firebase.messaging().getToken().then(function(currentToken){
+    if(currentToken){
+      console.log('Got FCM device token: ', currentToken);
+      //Savig the Device Token to the datastore.
+      firebase.firestore().collection('fcmTokens').doc(currentToken)
+        .set({uiid:firebase.auth().currentUser.uid});
+    }else{
+      requestNotificationsPermissions();
+    }
+  }).catch(function(error){
+    console.error('Unable to get messaging token.', error);
+  });
 }
 
 // Requests permissions to show notifications.
 function requestNotificationsPermissions() {
   // TODO 11: Request permissions to send notifications.
+  console.log('Requesting notifications permission...');
+  firebase.messaging().requestPermission().then(function(){
+    saveMessagingDeviceToken();
+  }).catch(function(error){
+    console.error('Unable to get permission to notify.', error);
+  });
 }
 
 // Triggered when a file is selected via the media picker.
@@ -317,6 +382,7 @@ mediaCaptureElement.addEventListener('change', onMediaFileSelected);
 initFirebaseAuth();
 
 // TODO: Enable Firebase Performance Monitoring.
+firebase.performance();
 
 // We load currently existing chat messages and listen to new ones.
 loadMessages();
